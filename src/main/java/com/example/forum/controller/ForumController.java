3f1c433a08160a593f421422a2a817f94e68a742
ogasawara_shoko <ogasawara.shoko@alhinc.jp>
2025-07-03 17:52:23 +0900

    first commit

---
+++ src/main/java/com/example/forum/controller/ForumController.java
@@ -0,0 +1,95 @@
+package com.example.forum.controller;
+
+import com.example.forum.controller.form.ReportForm;
+import com.example.forum.service.ReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class ForumController {
+    @Autowired
+    ReportService reportService;
+
+    /*
+     * 投稿内容表示処理
+     */
+    @GetMapping
+    public ModelAndView top() {
+        ModelAndView mav = new ModelAndView();
+        // 投稿を全件取得
+        List<ReportForm> contentData = reportService.findAllReport();
+        // 画面遷移先を指定
+        mav.setViewName("/top");
+        // 投稿データオブジェクトを保管
+        mav.addObject("contents", contentData);
+        return mav;
+    }
+
+    /*
+     * 新規投稿画面表示
+     */
+    @GetMapping("/new")
+    public ModelAndView newContent() {
+        ModelAndView mav = new ModelAndView();
+        // form用の空のentityを準備
+        ReportForm reportForm = new ReportForm();
+        // 画面遷移先を指定
+        mav.setViewName("/new");
+        // 準備した空のFormを保管
+        mav.addObject("formModel", reportForm);
+        return mav;
+    }
+
+    /*
+     * 新規投稿処理
+     */
+    @PostMapping("/add")
+    public ModelAndView addContent(@ModelAttribute("formModel") ReportForm reportForm) {
+        // 投稿をテーブルに格納
+        reportService.saveReport(reportForm);
+        // rootへリダイレクト
+        return new ModelAndView("redirect:/");
+    }
+
+    /*
+     * 投稿編集画面表示
+     */
+    @GetMapping("/edit")
+    public ModelAndView editContent() {
+        ModelAndView mav = new ModelAndView();
+        // form用の空のentityを準備
+        ReportForm reportForm = new ReportForm();
+        // 画面遷移先を指定
+        mav.setViewName("/edit");
+        // 準備した空のFormを保管
+        mav.addObject("formModel", reportForm);
+        return mav;
+    }
+
+    /*
+     * 投稿編集処理
+     */
+    @Mapping("/update")
+    public ModelAndView updateContent(@PathVariable Integer id) {
+        // 投稿をテーブルに格納
+        reportService.saveReport(id);
+        // rootへリダイレクト
+        return new ModelAndView("redirect:/");
+    }
+
+    /*
+     * 投稿削除処理
+     */
+    @DeleteMapping("/delete/{id}")
+    public ModelAndView deleteContent(@PathVariable Integer id) {
+        // 削除する投稿をテーブルに格納
+        reportService.deleteReport(id);
+        // rootへリダイレクト
+        return new ModelAndView("redirect:/");
+    }
+
+}
